6a807ff74703725787c1c263e1336ca48da3c98e
joyoonhoo <showil20@naver.com>
2023-06-09 19:09:17 +0900

    c

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -6,17 +6,17 @@ function App() {
 
     let logo = "React Blog";
     let [title , a] = useState(["남자코트 추천","강남 우동 맛집","react 독학"]);
-    let [up] = useState(0);
- 
+    let [count, countUp] = useState(0);
     
     return (
         
         <div className="App">
+            <button className='button' onClick={()=> {a(["여자코트 변경", "강남 우동 맛집", "react 독학"])}}>버튼</button>
             <div className='black-nav'>
                 <h4>{logo}</h4>
             </div>
             <div className='list'>
-                <h4>{title[0]} <span onClick={ console.log("up test")} className='btn'>❤️</span> {up++}</h4>
+                <h4>{title[0]} <span onClick={()=>{countUp(count + 1)}}>❤️</span> {count}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className='list'>
