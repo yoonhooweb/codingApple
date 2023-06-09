77f75732abee348024b0d2a0d0e30de41dafdc6b
joyoonhoo <showil20@naver.com>
2023-06-09 17:03:45 +0900

    useState

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let logo = "React Blog";
+    let [title , a] = useState(["남자코트 추천","강남 우동 맛집","react 독학"]);
+    let [up] = useState(0);
+ 
+    
+    return (
+        
+        <div className="App">
+            <div className='black-nav'>
+                <h4>{logo}</h4>
+            </div>
+            <div className='list'>
+                <h4>{title[0]} <span onClick={ console.log("up test")} className='btn'>❤️</span> {up++}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{title[1]} <span >❤️</span> 0 개</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{title[2]} <span >❤️</span> 0 개</h4>
+                <p>2월 17일 발행</p>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
