b0bd5b58a1ac236c0a45ea7424fe6da68f3ef41f
joyoonhoo <showil20@naver.com>
2023-06-16 21:10:48 +0900

    c

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -5,31 +5,57 @@ import { useState } from 'react';
 function App() {
 
     let logo = "React Blog";
-    let [title , a] = useState(["남자코트 추천","강남 우동 맛집","react 독학"]);
+    let [title, reName] = useState(["남자코트 추천", "강남 우동 맛집", "react 독학"]);
     let [count, countUp] = useState(0);
-    
+    let [modal, setModal] = useState(false);
+
     return (
-        
+
         <div className="App">
-            <button className='button' onClick={()=> {a(["여자코트 변경", "강남 우동 맛집", "react 독학"])}}>버튼</button>
             <div className='black-nav'>
                 <h4>{logo}</h4>
             </div>
+
+            <button className='button' onClick={() => {
+                let copy = [...title];
+                copy[0] = "코트 추천";
+                reName(copy);
+            }}>글 수정</button>
+
+            <button onClick={() => {
+                let sorts = [...title].sort();
+                reName(sorts);
+            }}>정렬</button>
             <div className='list'>
-                <h4>{title[0]} <span onClick={()=>{countUp(count + 1)}}>❤️</span> {count}</h4>
+                <h4>{title[0]} <span onClick={() => { countUp(count + 1) }}>❤️</span> {count}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className='list'>
-                <h4>{title[1]} <span >❤️</span> 0 개</h4>
+                <h4>{title[1]} <span>❤️</span> 0 개</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className='list'>
-                <h4>{title[2]} <span >❤️</span> 0 개</h4>
+                <h4>{title[2]} <span onClick={ () => { modal == true ? setModal(false) : setModal(true) }} >❤️</span> 0 개</h4>
                 <p>2월 17일 발행</p>
             </div>
+
+            
+            {
+                modal == true ? <Modal /> : ''
+            }
         </div>
 
     );
+}
+
+function Modal() {
+    return (
+        <div className="modal">
+            <h4>제목</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    )
 }
 
 export default App;
